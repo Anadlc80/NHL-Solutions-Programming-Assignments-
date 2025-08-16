e630d0c882d529d0b866e65e207c14fa3e42bd1f
Anadlc80 <anadlc80@gmail.com>
2025-08-16 21:06:13 +0100

    exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -1,7 +1,9 @@
-# There are two simple mistakes in this code: 1) you are using wrong number of colum using the column 1 for both columns the 0 and 1.
-# And the most important one, you are losing the information of several column when you give them other values. 
-# And as we now, we need to use a list of index, because Numpy evaluate first the right side and safe it in the temporal buffer. 
+# There are two simple mistakes in this code:
+# 1) you are using wrong number of colum, there is a repeticion of the column 1 for both columns the 0 and 1.
+# 2) And the most important one, you are losing the information of several column when you give them the values of other colums.
 # For example if you introduce in the colum 0 the values of colum 1 you already lost the values of colum 0
+# And as we now, we need to use a list of index, because Numpy evaluate first the right side and safe it in the temporal buffer. 
+# We use the list to safe the value of all colum and don't lost any value
 
 def swap(coords: np.ndarray):
         coords[:,[0,1,2,3]]= coords[:,[1,0,3,2]]

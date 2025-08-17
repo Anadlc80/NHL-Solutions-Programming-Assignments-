208d3e2cb8f80b03c36e882f240740109c052cbb
Anadlc80 <anadlc80@gmail.com>
2025-08-17 17:21:11 +0100

    exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,11 +1,18 @@
-# In the first exercise the problem was the type of structure used. The programm try to use an index in a Set estructure
+# --------------------------------------------------------------------------------------------------------------------------
+# Ana de la Cerda Galvan ------- Exercise 31 - NHL REQUERIMENTS  ------------------------------------------------------------
+# There is some Spanish code inside, I introduced them while I studied the code to be clear of what id does en every step
+#---------------------------------------------------------------------------------------------------------------------------
+#  BUG
+# The problem in this exercise was the type of structure used. The programm try to use an index in a Set estructure
 # The Set estructure doesn't have any specific orden and we can't access to its elems with an index. As result every time that we call the function we will have an aleatory fruit.
-
 # My solution trying to keep the same structure of the original program. I only convert it to an ordened list. After that with a loop I will look for the same index.
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# IMPROVE
 # But for me, I will use a more efficient way, I would just convert the set in an orden list and access to it directly with ther index. So we wouldn't need the loop at all.
 #      lista_ordenada=sorted(fruits)
 #      return lista_ordenada[fruit_id]
 # I wasn't sure how much I was allowed to change the code, but I let you know the both options.
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 def id_to_fruit(fruit_id: int, fruits:[str]) -> str:

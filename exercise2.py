761ee18957e547c3946ccac5c14499afc28a89a6
Anadlc80 <anadlc80@gmail.com>
2025-08-17 17:19:09 +0100

    exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -1,9 +1,15 @@
+# --------------------------------------------------------------------------------------------------------------------------
+# Ana de la Cerda Galvan ------- Exercise 2 - NHL REQUERIMENTS  ------------------------------------------------------------
+# There is some Spanish code inside, I introduced them while I studied the code to be clear of what id does en every step
+#---------------------------------------------------------------------------------------------------------------------------
+# STRUCTURE BUG
 # There are two simple mistakes in this code:
 # 1) you are using wrong number of colum, there is a repeticion of the column 1 for both columns the 0 and 1.
 # 2) And the most important one, you are losing the information of several column when you give them the values of other colums.
 # For example if you introduce in the colum 0 the values of colum 1 you already lost the values of colum 0
 # And as we now, we need to use a list of index, because Numpy evaluate first the right side and safe it in the temporal buffer. 
 # We use the list to safe the value of all colum and don't lost any value
+#----------------------------------------------------------------------------------------------------------------------------------------
 
 def swap(coords: np.ndarray):
         coords[:,[0,1,2,3]]= coords[:,[1,0,3,2]]
